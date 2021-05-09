267c16e0eb1cd162ad6a4688e9ac5843ff344e17
lyhuynhvu <lyhuynhvu2906@gmail.com>
2021-05-09 13:19:38 +0700

    new_task2 done

--- src/com/cybersoft/home/MainApp.java
+++ src/com/cybersoft/home/MainApp.java
@@ -3,6 +3,7 @@ package com.cybersoft.home;
 public class MainApp {
 	public static void main(String[] args) {
 		System.out.println("new task done");
-		System.out.println("hello");
+		System.out.println("new task1 done");
+		System.out.println("new task2 done");
 	}
 }
